1e84227563813e3a1c20cd1e424ab0fb9a90ed24
Nick Mugo <nicknjuru13@gmail.com>
2023-10-14 18:18:01 +0300

    Completed assignment

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,30 @@
-#!/usr/bin/env python3
-
 class MyString:
-  pass
+    def __init__(self, value=""):
+        if not isinstance(value, str):
+            print("The value must be a string.")
+            self.value = ""
+        else:
+            self.value = value
+
+    def is_sentence(self):
+        if self.value.endswith("."):
+            return True
+        return False
+
+    def is_question(self):
+        if self.value.endswith("?"):
+            return True
+        return False
+
+    def is_exclamation(self):
+        if self.value.endswith("!"):
+            return True
+        return False
+
+    def count_sentences(self):
+        if not self.value:
+            return 0
+
+        sentences = self.value.split('.')
+        sentences = [s for s in sentences if s]  # Remove empty strings
+        return len(sentences)
